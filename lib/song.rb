557a88a578e640f466dcf765de77e8874334d71d
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-06-01 01:38:19 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,8 @@
 require 'pry'
 class Song
-  attr_accessor :name, :artist_name
+
+attr_accessor :name, :artist_name
+  
   @@all = []
   
   def self.all
@@ -17,57 +19,57 @@ class Song
     self.class.all << self
   end
 
-def self.new_by_name(song_name)
-  song = self.new
-  song.name = song_name
-  song
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
  end
  
-def self.create_by_name(song_name)
-  song = self.new
-  song.name = song_name
-  song.save
-  song
-end
-
-def self.find_by_name(song_name)
- @@all.detect do |song| 
-   song_name == song.name
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song.save
+    song
   end
- end
- 
-def self.find_or_create_by_name(song_name)
- if self.find_by_name(song_name)
-    self.find_by_name(song_name)
- else
-    self.create_by_name(song_name)
+
+  def self.find_by_name(song_name)
+    @@all.detect do |song| 
+    song_name == song.name
+    end
   end
- end
  
-def self.alphabetical
-  self.all.sort_by {|song| song.name}
- end 
+  def self.find_or_create_by_name(song_name)
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else
+      self.create_by_name(song_name)
+    end
+  end
+ 
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end 
 
-def self.new_from_filename(file_name)
- file = file_name.split(" - ").collect{|s| s.chomp(".mp3")}
- song = self.new
- song.artist_name = file[0] 
- song.name = file[1]
- song
-end
+  def self.new_from_filename(file_name)
+    file = file_name.split(" - ").collect{|s| s.chomp(".mp3")}
+    song = self.new
+    song.artist_name = file[0] 
+    song.name = file[1]
+    song
+  end
 
-def self.create_from_filename(file_name)
- file = file_name.split(" - ").collect{|s| s.chomp(".mp3")}
- song = self.new
- song.artist_name = file[0] 
- song.name = file[1]
- song.save
- song
-end
+  def self.create_from_filename(file_name)
+    file = file_name.split(" - ").collect{|s| s.chomp(".mp3")}
+    song = self.new
+    song.artist_name = file[0] 
+    song.name = file[1]
+    song.save
+    song
+  end
 
-def self.destroy_all
-  @@all = []
-  @@all.clear
-end
+  def self.destroy_all
+    @@all = []
+    @@all.clear
+  end
 end
 
